ea6906cd989256c588d83047e2ddb4a388b2f68b
Oliver Crawford <oicrawford@gmail.com>
2024-03-02 23:50:09 -0600

    Removed unused cases in the DisplayContent method in ADPMainMenu

--- ADPSemesterProject/ADPMainMenu.cs
+++ ADPSemesterProject/ADPMainMenu.cs
@@ -119,28 +119,15 @@ namespace ADPSemesterProject
         {
             switch (collectionName)
             {
-                case "menuCollection":
-                    List<Menu> menuList = menuCollection.AsQueryable().ToList();
-                    dataGridView1.DataSource = menuList;
-                    break;
-                case "ordersCollection":
-                    List<Orders> ordersList = ordersCollection.AsQueryable().ToList();
-                    dataGridView1.DataSource = ordersList;
-                    break;
                 case "staffCollection":
                     List<Staff> staffList = staffCollection.AsQueryable().ToList();
                     dataGridView1.DataSource = staffList;
-                    break;
-                case "tablesCollection":
-                    List<Tables> tablesList = tablesCollection.AsQueryable().ToList();
-                    dataGridView1.DataSource = tablesList;
                     break;
                 case "filteredSCUserOnly":
                     var builderC2 = Builders<Staff>.Filter;
                     var filterC2 = builderC2.Eq("Name", username) & builderC2.Eq("Password", password);
                     List<Staff> filteredUserOnlyList = staffCollection.Find(filterC2).ToList();
                     dataGridView1.DataSource = filteredUserOnlyList;
-
                     break;
                 default:
                     MessageBox.Show("No known collection called " + collectionName);
